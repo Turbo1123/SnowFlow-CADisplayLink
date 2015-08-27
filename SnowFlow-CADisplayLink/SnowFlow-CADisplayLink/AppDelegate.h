e6aae18741c43d7766df21ca156c98f8c33c4f3b
liyunlong1123 <liyunlong3189@163.com>
2015-08-27 15:31:41 +0800

    init code

---
+++ SnowFlow-CADisplayLink/SnowFlow-CADisplayLink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnowFlow-CADisplayLink
+//
+//  Created by 李云龙 on 15/8/27.
+//  Copyright (c) 2015年 hihilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnowFlow-CADisplayLink/SnowFlow-CADisplayLink/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SnowFlow-CADisplayLink
+//
+//  Created by 李云龙 on 15/8/27.
+//  Copyright (c) 2015年 hihilong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
